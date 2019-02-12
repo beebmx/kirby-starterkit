9d995cc89b2ab6f17447674b560f6343d3f82eae
Fernando Gutierrez <fernando@beeb.mx>
2019-02-12 12:28:11 -0600

    Update configuration

--- site/config/config.php
+++ site/config/config.php
@@ -8,7 +8,24 @@ $storage = $base . '/storage';
 
 return [
     'debug' => env('KIRBY_DEBUG', false),
+    'panel' => [
+        'install' => env('KIRBY_INSTALL', false),
+        'slug' => env('KIRBY_PANEL', 'panel')
+    ],
+    'api' => env('KIRBY_API', true),
+    'cookieName' => env('KIRBY_SESSION', 'kirby_session'),
     'hooks' => require_once 'hooks.php',
     'routes' => require_once 'routes.php',
     'beebmx.kirby-blade.views' => $storage . '/views',
+    'email' => [
+        'transport' => [
+            'type' => env('MAIL_DRIVER', 'mail'),
+            'host' => env('MAIL_HOST', 'smtp.server.com'),
+            'port' => env('MAIL_PORT', 465),
+            'security' => env('MAIL_ENCRYPTION', 'tls'),
+            'auth' => env('MAIL_AUTH', false),
+            'username' => env('MAIL_USERNAME', ''),
+            'password' => env('MAIL_PASSWORD', ''),
+        ]
+    ]
 ];
