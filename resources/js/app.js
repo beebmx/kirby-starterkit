f50c7d271131b8436d01d039b58893616fc72f91
Fernando Gutierrez <fernando@beeb.mx>
2019-03-21 19:49:22 -0600

    Update packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,8 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Uncomment if you need Vue
- * 
- * Vue.component('example', require('./components/Example.vue').default);
- * 
- * new Vue({
- *     el: '#app'
- * });
- */
+// Uncomment if you need Vue
+// Vue.component('example', require('./components/Example.vue').default);
+// new Vue({
+//     el: '#app'
+// });
