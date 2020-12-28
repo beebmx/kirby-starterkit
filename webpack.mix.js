89f22f530e8f12e8aebe4959951947e63d962cac
Fernando Gutierrez <fernando@beeb.mx>
2020-12-27 18:12:19 -0600

    Update laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,13 +11,14 @@ let mix = require('laravel-mix');
  */
 
 mix
-  .js('resources/js/app.js', 'public/js')
-  .postCss('resources/css/app.css', 'public/css', [
+  .js('resources/js/app.js', 'js')
+  .postCss('resources/css/app.css', 'css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-nested'),
   ])
   .setPublicPath('public/')
+  .disableSuccessNotifications()
 
 if (mix.inProduction()) {
     mix.version()
