03006a6f868ca7399f83b5a2bc61f95da7bbd0b0
Fernando Gutierrez <fernando@beeb.mx>
2021-06-11 19:51:50 -0500

    Update packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 require('./bootstrap');
 
 // Uncomment if you need Alpine.js
-// import 'alpinejs'
+// import Alpine from 'alpinejs'
+// window.Alpine = Alpine
+// Alpine.start()
 
 // Uncomment if you need Vue
 // window.Vue = require('vue');
