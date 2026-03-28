9c50c505ce7551533d8c4f8156d1ec78483a83c2
Fernando Gutierrez <fernando@beeb.mx>
2026-03-28 13:19:42 -0600

    1.3.1
    - Added new block Example
    - Fixed tests resources

---
+++ site/plugins/app/resources/components/blocks/Example.vue
@@ -0,0 +1,19 @@
+<script setup>
+  const props = defineProps({
+    content: Object,
+    disabled: Boolean,
+    endpoints: Object,
+    fieldset: Object,
+    id: String,
+    isHidden: Boolean,
+    type: String,
+  })
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+  /**/
+</style>
